466b434ee3a0d715a56a850e3f754598ec36666a
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-15 14:46:20 +0600

    form ui updated

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -2,10 +2,10 @@ import { currencyFormatter } from "../utils/currencyFormatter";
 
 const ProjectDetails = ({ project }) => {
   return (
-    <div className="project bg-slate-800 p-5 rounded-xl shadow-xl border border-slate-700 flex flex-col gap-5">
+    <div className="project bg-slate-800 p-5 rounded-xl shadow-xl border border-slate-700 flex flex-col gap-5 w-[30rem]">
       <div className="project-top">
         <span className="text-sky-400">ID: {project._id}</span>
-        <h3 className="text-3xl font-medium">{project.title}</h3>
+        <h3 className="text-3xl font-medium truncate">{project.title}</h3>
         <span className="uppercase text-xs tracking-widest font-medium text-slate-500">
           {project.tech}
         </span>
--- src/components/ProjectFrom.js
+++ src/components/ProjectFrom.js
@@ -14,7 +14,6 @@ const ProjectFrom = () => {
 
     const project = { title, tech, budget, duration, manager, dev };
 
-    // posting project to the server
     const res = await fetch("http://localhost:4000/api/projects", {
       method: "POST",
       headers: {
@@ -46,69 +45,114 @@ const ProjectFrom = () => {
         Add a New Project
       </h2>
 
-      <div className="form-control">
-        <label htmlFor="title">Project Title</label>
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="title"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Project Title
+        </label>
         <input
           type="text"
           placeholder="e.g. e-commerce store"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           id="title"
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300"
         />
       </div>
-      <div className="form-control">
-        <label htmlFor="tech">Technologies</label>
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="tech"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Technologies
+        </label>
         <input
           type="text"
           placeholder="e.g. node, express, react"
           value={tech}
           onChange={(e) => setTech(e.target.value)}
           id="tech"
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300"
         />
       </div>
-      <div className="form-control">
-        <label htmlFor="budget">Budget (in USD)</label>
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="budget"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Budget (in USD)
+        </label>
         <input
           type="number"
           placeholder="e.g. 1000"
           value={budget}
           onChange={(e) => setBudget(e.target.value)}
           id="budget"
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300"
         />
       </div>
-      <div className="form-control">
-        <label htmlFor="duration">Duration (in weeks)</label>
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="duration"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Duration (in weeks)
+        </label>
         <input
           type="number"
           placeholder="e.g. 4"
           value={duration}
           onChange={(e) => setDuration(e.target.value)}
           id="duration"
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300"
         />
       </div>
-      <div className="form-control">
-        <label htmlFor="manager">Manager</label>
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="manager"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Manager
+        </label>
         <input
           type="text"
           placeholder="e.g. Sarah"
           value={manager}
           onChange={(e) => setManager(e.target.value)}
           id="manager"
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300"
         />
       </div>
-      <div className="form-control">
-        <label htmlFor="dev">Total Developers</label>
+      <div className="form-control flex flex-col gap-2">
+        <label
+          htmlFor="dev"
+          className="cursor-pointer hover:text-sky-400 duration-300"
+        >
+          Total Developers
+        </label>
         <input
           type="number"
           placeholder="e.g. 5"
           value={dev}
           onChange={(e) => setDev(e.target.value)}
           id="dev"
+          className="bg-transparent border border-slate-500 py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300"
         />
       </div>
 
-      <button type="submit">Add Project</button>
-      {error && <p className="error">{error}</p>}
+      <button
+        type="submit"
+        className="bg-sky-400 text-slate-900 py-3 rounded-lg hover:bg-sky-50 duration-300"
+      >
+        Add Project
+      </button>
+      {error && (
+        <p className="error bg-rose-500/10 p-5 rounded-lg border border-rose-500 text-rose-500">
+          {error}
+        </p>
+      )}
     </form>
   );
 };
