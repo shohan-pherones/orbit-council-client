dbd2b94a34acdb7daa58b217a0df328f058d60be
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-19 20:14:41 +0600

    login hook added

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const { dispatch } = useAuthContext();
+
+  const login = async (email, password) => {
+    setLoading(true);
+    setError(null);
+
+    const res = await fetch(
+      process.env.REACT_APP_BASE_URL + "/api/user/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }
+    );
+
+    const json = await res.json();
+
+    if (!res.ok) {
+      setLoading(false);
+      setError(json.error);
+    }
+
+    if (res.ok) {
+      // save the user to local storage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      // update the auth context
+      dispatch({ type: "LOGIN", payload: json });
+
+      setLoading(false);
+    }
+  };
+
+  return { login, loading, error };
+};
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { login, loading, error } = useLogin();
+
   const handleLogin = async (e) => {
     e.preventDefault();
+
+    await login(email, password);
   };
 
   return (
@@ -54,9 +59,16 @@ const Login = () => {
       <button
         type="submit"
         className="bg-sky-400 text-slate-900 py-3 rounded-lg hover:bg-sky-50 duration-300"
+        disabled={loading}
       >
         Log in
       </button>
+
+      {error && (
+        <p className="error bg-rose-500/10 p-5 rounded-lg border border-rose-500 text-rose-500">
+          {error}
+        </p>
+      )}
     </form>
   );
 };
