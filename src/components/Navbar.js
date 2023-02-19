e2c0b0c50f2fe603a6c5d9eecc79e59de780fb3e
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-19 20:26:35 +0600

    persist user auth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleLogout = () => {
     logout();
@@ -18,23 +20,28 @@ const Navbar = () => {
       </Link>
 
       <nav className="flex items-center gap-5">
-        <div className="flex gap-5">
-          <Link to="/login" className="hover:text-sky-400 duration-300">
-            Login
-          </Link>
-          <Link to="/signup" className="hover:text-sky-400 duration-300">
-            Signup
-          </Link>
-        </div>
+        {!user && (
+          <div className="flex gap-5">
+            <Link to="/login" className="hover:text-sky-400 duration-300">
+              Login
+            </Link>
+            <Link to="/signup" className="hover:text-sky-400 duration-300">
+              Signup
+            </Link>
+          </div>
+        )}
 
-        <div>
-          <button
-            onClick={handleLogout}
-            className="bg-rose-500 py-2 px-5 rounded shadow-xl hover:bg-rose-50 hover:text-slate-900 duration-300"
-          >
-            Logout
-          </button>
-        </div>
+        {user && (
+          <div className="flex gap-5 items-center">
+            <span>{user.email}</span>
+            <button
+              onClick={handleLogout}
+              className="bg-rose-500 py-2 px-5 rounded shadow-xl hover:bg-rose-50 hover:text-slate-900 duration-300"
+            >
+              Logout
+            </button>
+          </div>
+        )}
       </nav>
     </div>
   );
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 const initialState = {
   user: null,
@@ -26,7 +26,13 @@ export const AuthContext = createContext();
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
-  console.log("AuthContext state: ", state);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
